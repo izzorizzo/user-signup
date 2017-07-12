36a8a625533ffac04cbb752510232c25acf74998
Isabela Oliveira <isabela.j.oliveira@gmail.com>
2017-07-11 21:09:34 -0700

    fixed redirect and validation issues

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def signup():
         password = request.form["password"]
         verify = request.form["verify"]
         email = request.form["email"]
+        
 
         #empty strings for errors 
         username_error = ""
@@ -22,18 +23,16 @@ def signup():
         verify_error = "" 
         email_error = ""
 
+
         # checks length of username
-        if len(username) >=3 and len(username) <20:
-            return True
-        else:
+        if len(username) <=3 or len(username) >=20:
             username_error = "Please input a username between 3 and 20 characters." 
             # wipe out username if wrong
             username = ""
+
             
         # checks length of password
-        if len(password) >=3 and len(password) <20:
-            return True
-        else:
+        if len(password) <=3 or len(password) >=20:
             password_error = "Please input a password between 3 and 20 characters." 
             # wipe out password fields if error
             password = ""
@@ -41,9 +40,7 @@ def signup():
 
 
         # checks passwords match
-        if password == verify:
-            return True
-        else:
+        if password != verify:
             verify_error = "Passwords do not match."
             # wipe out if passwords don't match
             password = ""
@@ -51,18 +48,24 @@ def signup():
 
 
         # validates email
-        if (len(email) >=3 and len(email) <20) and "@" in email and "." in email and " " not in email: 
-            return True
-        else: 
-            email_error = "Please input valid email address that is between 3 and 20 characters long."
+        if len(email) <=3 or len(email) >=20:
+            email_error = "Please input a valid email address that has between 3 and 20 characters long, and contains a '@' symbol, a '.', and has no spaces."
             # wipes out email field
             email = ""
 
+
+        if "@" not in email or "." not in email or " " in email:
+            email_error = "Please input a valid email address that has between 3 and 20 characters long, and contains a '@' symbol, a '.', and has no spaces."
+            # wipes out email field
+            email = ""
+
+
         #redirects if no errors show up
         if (username_error == "") and (password_error == "") and (verify_error == "") and (email_error == ""):
-            return redirect("/welcome.html?username={0}".format(username))
+            return redirect("/welcome?username={0}".format(username))
         else: 
             return render_template("signup.html", title="Signup", username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, username=username, password=password, verify=verify, email=email) 
+
 
     return render_template("signup.html", title="Signup")
 
